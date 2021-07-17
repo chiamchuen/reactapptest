c0ce24a5327ff4c8948e67a7679b8e22bbe887fb
himreaowi <himariang@gmail.com>
2021-07-17 14:28:55 +0800

    question1

---
+++ assets/colors/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    lightGreen: '#CADB5F',
+    darkGreen: '#A7B93C',
+    midGreen: '#ABC934',
+    backgroundGreen: '#F8FFD0',
+    darkestGreen: '#7F9215',
+    doctorbackground: '#CCF5F5',
+    doctorbutton: '#4AA0A0',
+    input: '#F2F2F2',
+};
+
+export default colors;
---
+++ components/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default Home = () => {
+    return <Text>Home page</Text>;
+};
