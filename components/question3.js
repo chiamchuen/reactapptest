72ab9e6fae716c545c5eba8966905503c4aed117
bluemaple78 <angraysean12@gmail.com>
2021-07-28 00:45:58 +0800

    Update question3.js

--- components/question3.js
+++ components/question3.js
@@ -10,6 +10,7 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { Triangle } from 'react-native-shape';
 import Checkbox from 'expo-checkbox';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 export default function question3 ({ navigation }) {
     let[fontsLoaded, error]=useFonts({
@@ -18,7 +19,12 @@ export default function question3 ({ navigation }) {
         'Montserrat-Bold':require('../assets/fonts/Montserrat-Bold.ttf'),
         'Montserrat-Regular':require('../assets/fonts/Montserrat-Regular.ttf')
       });
-
+      const [checkboxState, setCheckboxState] = React.useState(false);
+      const [checkboxState2, setCheckboxState2] = React.useState(false);
+      const [checkboxState3, setCheckboxState3] = React.useState(false);
+      const [checkboxState4, setCheckboxState4] = React.useState(false);
+      const [checkboxState5, setCheckboxState5] = React.useState(false);
+      const [checkboxState6, setCheckboxState6] = React.useState(false);
     if (!fontsLoaded){
         return <AppLoading />
     }
@@ -44,7 +50,78 @@ export default function question3 ({ navigation }) {
       <View style={styles.questionWrapper}>
           <Text style={styles.questionContent}>Do you have the symptoms listed below?</Text>
       </View>
+        
+      <View style={styles.optionsWrapper}>
+          <BouncyCheckbox
+            isChecked={checkboxState}
+            disableBuiltInState
+            onPress={() => setCheckboxState(!checkboxState)} 
+            fillColor={colors.darkestGreen} 
+            iconStyle={{ borderColor: colors.darkestGreen, borderRadius: 6, height: 23, width: 23, borderWidth: 1, }} 
+            text = "Headache" 
+            textStyle={{ fontFamily: 'Lato-Regular', textDecorationLine: 'none', }}
+          />
+        </View>
 
+        <View style={styles.optionsWrapper2}>
+          <BouncyCheckbox 
+            isChecked={checkboxState2}
+            disableBuiltInState
+            onPress={() => setCheckboxState2(!checkboxState2)} 
+            fillColor={colors.darkestGreen} 
+            iconStyle={{ borderColor: colors.darkestGreen, borderRadius: 6, height: 23, width: 23, borderWidth: 1, }} 
+            text = "Fever" 
+            textStyle={{ fontFamily: 'Lato-Regular', textDecorationLine: 'none', }}
+          />
+        </View>
+
+        <View style={styles.optionsWrapper2}>
+          <BouncyCheckbox
+            isChecked={checkboxState3}
+            disableBuiltInState
+            onPress={() => setCheckboxState3(!checkboxState3)} 
+            fillColor={colors.darkestGreen} 
+            iconStyle={{ borderColor: colors.darkestGreen, borderRadius: 6, height: 23, width: 23, borderWidth: 1, }} 
+            text = "Fatigue" 
+            textStyle={{ fontFamily: 'Lato-Regular', textDecorationLine: 'none', }}
+          />
+        </View>
+
+        <View style={styles.optionsWrapper2}>
+          <BouncyCheckbox
+            isChecked={checkboxState4}
+            disableBuiltInState
+            onPress={() => setCheckboxState4(!checkboxState4)} 
+            fillColor={colors.darkestGreen} 
+            iconStyle={{ borderColor: colors.darkestGreen, borderRadius: 6, height: 23, width: 23, borderWidth: 1, }} 
+            text = "Chills" 
+            textStyle={{ fontFamily: 'Lato-Regular', textDecorationLine: 'none', }}
+          />
+        </View>
+
+        <View style={styles.optionsWrapper2}>
+          <BouncyCheckbox
+            isChecked={checkboxState5}
+            disableBuiltInState
+            onPress={() => setCheckboxState5(!checkboxState5)} 
+            fillColor={colors.darkestGreen} 
+            iconStyle={{ borderColor: colors.darkestGreen, borderRadius: 6, height: 23, width: 23, borderWidth: 1, }} 
+            text = "Runny Nose" 
+            textStyle={{ fontFamily: 'Lato-Regular', textDecorationLine: 'none', }}
+          />
+        </View>
+
+        <View style={styles.optionsWrapper2}>
+          <BouncyCheckbox
+            isChecked={checkboxState6}
+            disableBuiltInState
+            onPress={() => setCheckboxState6(!checkboxState6)} 
+            fillColor={colors.darkestGreen} 
+            iconStyle={{ borderColor: colors.darkestGreen, borderRadius: 6, height: 23, width: 23, borderWidth: 1, }} 
+            text = "Nausea" 
+            textStyle={{ fontFamily: 'Lato-Regular', textDecorationLine: 'none', }}
+          />
+        </View>
       {/* back and next */}
       <View style={styles.bottomWrapper}>
         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backWrapper}>
@@ -123,14 +200,80 @@ const styles = StyleSheet.create({
       fontFamily: 'Lato-Bold',
       color: '#fff',
       paddingTop: 12,
-      paddingLeft: 19,
+      paddingLeft: 15,
       alignItems: 'center',
     },
+
+    optionsWrapper: {
+      marginTop: 40,
+      marginHorizontal: 90,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+  
+    optionsBox: {
+      height: 23,
+      width: 23,
+      borderRadius: 6,
+      borderColor: colors.darkestGreen,
+      borderWidth: 1,
+    },
+  
+    optionsText: {
+      fontFamily: 'Lato-Regular',
+      fontSize: 16,
+      marginHorizontal: 15,
+      color: '#707070',
+    },
+  
+    optionsWrapper2: {
+      marginTop: 35,
+      marginHorizontal: 90,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+
+    radioButtonContainer: {
+      marginTop: 40,
+      marginHorizontal: 42,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+  
+    radioButton: {
+      height: 20,
+      width: 20,
+      backgroundColor: "#F8F8F8",
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: "#E6E6E6",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+
+    radioButtonIconContainer: {
+      flexDirection: 'row',
+
+    },
+  
+    radioButtonIcon: {
+      height: 14,
+      width: 14,
+      borderRadius: 7,
+      backgroundColor: colors.darkestGreen,
+    },
+  
+    radioButtonText: {
+      fontFamily: 'Lato-Regular',
+      fontSize: 16,
+      marginHorizontal: 15,
+      color: '#707070',
+    },    
 
     bottomWrapper: {
       flexDirection: 'row',
       alignItems: 'center',
-      marginTop: 300,
+      marginTop: 30,
     },
 
     backWrapper: {
